f9aae1250fcd1115d26d2cbe8c14fe5f64fc6025
Documenter.jl <documenter@juliadocs.github.io>
2023-01-03 16:54:18 +0000

    build based on eb1b1d7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SkeelBerzins.jl","page":"SkeelBerzins.jl","title":"SkeelBerzins.jl","text":"","category":"section"},{"location":"","page":"SkeelBerzins.jl","title":"SkeelBerzins.jl","text":"Documentation for SkeelBerzins.jl","category":"page"}]
+}
