6b125db5bb4666463b918605c7bfe2e24e971110
gregoirepourtier <gpourtier@icloud.com>
2023-11-17 16:32:22 +0100

    remove :banded Tests for Julia 1.6

--- examples/Example101_LinearDiffusion.jl
+++ examples/Example101_LinearDiffusion.jl
@@ -70,10 +70,8 @@ function main()
 	end
 
 	params_DiffEq             = SkeelBerzins.Params(solver=:DiffEq,sparsity=:sparseArrays)
-	params_DiffEq_banded      = SkeelBerzins.Params(solver=:DiffEq,sparsity=:banded)
 
 	sol_DiffEq             = solve_problem(m,pdefun,icfun,bdfun,x_mesh,tspan,params_DiffEq)
-	sol_DiffEq_banded      = solve_problem(m,pdefun,icfun,bdfun,x_mesh,tspan,params_DiffEq_banded ; linsolve=LUFactorization())
 	sol_DiffEq_doubleFloat = solve_problem(m,pdefun,icfun,bdfun,x_mesh_doubleFloat,tspan,params_DiffEq ; linsolve=SparspakFactorization())
 
 	tstep = collect(0:1e-3:1)
@@ -82,11 +80,20 @@ function main()
 
 	sol_euler_vecTstep, hist = solve_problem(m,pdefun,icfun,bdfun,x_mesh,tspan,params_euler_vecTstep)
 	sol_euler_fixTstep       = solve_problem(m,pdefun,icfun,bdfun,x_mesh,tspan,params_euler_fixTstep)
-	sol_euler_banded         = pdepe(m,pdefun,icfun,bdfun,x_mesh,tspan ; solver=:euler, sparsity=:banded, tstep=1e-3, linsolve=LUFactorization())
 	sol_euler_doubleFloat    = pdepe(m,pdefun,icfun,bdfun,x_mesh_doubleFloat,tspan ; solver=:euler, tstep=1e-3, linsolve=SparspakFactorization())
 
-	return (sum(sol_DiffEq.u[end]),sum(sol_DiffEq_banded.u[end]),eltype(eltype(sol_DiffEq_doubleFloat.u)), 
-			sum(sol_euler_vecTstep.u[end]),sum(sol_euler_fixTstep.u[end]),sum(sol_euler_banded.u[end]),eltype(eltype(sol_euler_doubleFloat.u)))
+    if VERSION >= VersionNumber(1,9,0)
+        params_DiffEq_banded = SkeelBerzins.Params(solver=:DiffEq,sparsity=:banded)
+        sol_DiffEq_banded    = solve_problem(m,pdefun,icfun,bdfun,x_mesh,tspan,params_DiffEq_banded ; linsolve=LUFactorization())
+
+        sol_euler_banded         = pdepe(m,pdefun,icfun,bdfun,x_mesh,tspan ; solver=:euler, sparsity=:banded, tstep=1e-3, linsolve=LUFactorization())
+
+        return (sum(sol_DiffEq.u[end]),sum(sol_DiffEq_banded.u[end]),eltype(eltype(sol_DiffEq_doubleFloat.u)), 
+			    sum(sol_euler_vecTstep.u[end]),sum(sol_euler_fixTstep.u[end]),sum(sol_euler_banded.u[end]),eltype(eltype(sol_euler_doubleFloat.u)))
+    else
+        return (sum(sol_DiffEq.u[end]),eltype(eltype(sol_DiffEq_doubleFloat.u)), 
+			    sum(sol_euler_vecTstep.u[end]),sum(sol_euler_fixTstep.u[end]),eltype(eltype(sol_euler_doubleFloat.u)))
+    end
 end
 
 function test()
@@ -96,15 +103,17 @@ function test()
     testval_euler         = 3.721004873950427
     testval_euler_banded  = 3.7210048106612303
 
-	approx_diffEq, approx_diffEq_banded, sol_diffEq_doubleFloat, approx_euler_vec, approx_euler, approx_euler_banded, sol_euler_doubleFloat = main()
+    if VERSION >= VersionNumber(1,9,0)
+        approx_diffEq, approx_diffEq_banded, sol_diffEq_doubleFloat, approx_euler_vec, approx_euler, approx_euler_banded, sol_euler_doubleFloat = main()
 
-    if !Sys.iswindows() || VERSION >= VersionNumber(1,9,0)
         all_tests = approx_diffEq ≈ testval_diffEq && 
         sol_diffEq_doubleFloat == Double64 == sol_euler_doubleFloat &&
         approx_diffEq_banded ≈ testval_diffEq_banded && 
         approx_euler ≈ testval_euler ≈ approx_euler_vec &&
         approx_euler_banded ≈ testval_euler_banded
     else
+        approx_diffEq, sol_diffEq_doubleFloat, approx_euler_vec, approx_euler, sol_euler_doubleFloat = main()
+
         all_tests = approx_diffEq ≈ testval_diffEq && 
         sol_diffEq_doubleFloat == Double64 == sol_euler_doubleFloat &&
         approx_euler ≈ testval_euler ≈ approx_euler_vec
